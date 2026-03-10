b8bdccbc35219cc40ae2668c14192f04ed2d5f73
SurajShrestha2 <suraj.shrestha8520@gmail.com>
2026-03-11 01:42:04 +1100

    ss

--- server/index.js
+++ server/index.js
@@ -1,4 +1,4 @@
-require('dotenv').config({ path: require('path').join(__dirname, '.env') });
+require('dotenv').config();
 const express = require('express');
 const http = require('http');
 const { Server } = require('socket.io');
